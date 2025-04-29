3c93f9c748ed28f0e71064aab4537f0d5954bf7e
Roland <machodin@yandex.ru>
2025-04-29 03:39:50 +0300

    backend init

---
+++ backend/src/tasks/tasks.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: Repository<Task>
+  ) { }
+
+  async create(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = this.taskRepository.create(createTaskDto);
+    return await this.taskRepository.save(task);
+  }
+
+  async findAll(): Promise<Task[]> {
+    return await this.taskRepository.find();
+  }
+
+  async update(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    const task = await this.taskRepository.findOne({ where: { id } });
+    return await this.taskRepository.save({ ...task, ...updateTaskDto })
+  }
+}
